dc0d3e993849da1b7f788863c752ceb2ad0350ef
franco.g.padovani <franco.g.padovani@accenture.com>
2022-09-20 22:26:52 -0300

    Google signin added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const {generarJWT} = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
 
@@ -49,6 +50,51 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response) => {
+    const { id_token } = req.body;
+
+    try{
+        const {nombre, img, email} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ email });
+
+        if( ! usuario ){
+            const data = {
+                nombre,
+                email,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario (data);
+
+            await usuario.save(); 
+        }
+
+        if (!usuario.estado){
+            return res.status(401).json({
+                msg: 'Usuario bloqueado'
+            });
+        }
+
+        //generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (err){
+        res.status(400).json({
+            ok: false,
+            msg: 'el token no se pudo verificar'
+        })
+    }
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
